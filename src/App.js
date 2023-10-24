3ad23d5be60131cd6dfbc1ceac7688ff2e992ee3
Ahn jisoo <anjisu2001@naver.com>
2023-10-24 19:30:05 +0900

    card component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Main from "./Main";
+import "./style.css";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
